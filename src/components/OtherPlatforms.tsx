07050e20f3d5d01117fbdc8ed4104d643d9da46b
edsonarios <edsonrios9@gmail.com>
2024-02-28 11:30:47 -0400

    Added plugin tailwind animation, and improve other platforms icons and animation

--- src/components/OtherPlatforms.tsx
+++ src/components/OtherPlatforms.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { Windows, Mac, Linux, Download } from "../icons/Platforms";
+import { Windows, Mac, Download } from "../icons/Platforms";
 export function OtherPlatforms() {
   const [open, setOpen] = useState(true);
   return (
@@ -10,7 +10,7 @@ export function OtherPlatforms() {
             className="flex items-center sm:text-3xl font-medium title-font mb-4"
             onClick={() => setOpen(!open)}
           >
-            <h2 className="text-white text-lg mr-4">Other Platforms</h2>
+            <h2 className="text-white text-base mr-4">Other Platforms</h2>
             <svg
               width="22"
               height="13"
@@ -31,7 +31,7 @@ export function OtherPlatforms() {
         </header>
 
         {open && (
-          <div>
+          <div className="animate-slide-in-top">
             <div className="flex flex-col justify-center items-center mt-6 mb-10">
               <div className="w-16 h-1 rounded-full bg-indigo-500 inline-flex"></div>
               <h2 className="text-lg title-font font-medium mt-4 mb-3">
@@ -62,7 +62,7 @@ export function OtherPlatforms() {
                 </div>
               </div>
               <div className="p-4 md:w-1/3 flex flex-col text-center items-center">
-                <Linux />
+                <img src="/linux-logo.png" width={60} />
                 <div className="flex-grow mt-10">
                   <h2 className="text-lg title-font font-medium mb-3">Linux</h2>
                   <button className="mt-3 text-indigo-500 inline-flex items-center">
