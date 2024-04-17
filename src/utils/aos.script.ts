26cc5810a7f97ae97bf860e414effe64986e1165
edsonarios <edsonrios9@gmail.com>
2024-04-16 20:16:12 -0400

    Added section mainFeatures with aos effect

---
+++ src/utils/aos.script.ts
@@ -0,0 +1,10 @@
+import AOS from 'aos'
+import 'aos/dist/aos.css'
+
+document.addEventListener("DOMContentLoaded", function () {
+  AOS.init({
+    duration: 1000,
+    delay: 150,
+    // once: true,
+  })
+})
