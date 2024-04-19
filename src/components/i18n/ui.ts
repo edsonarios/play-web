bbe1e24411be1e365207160165fcb64f77ab42c1
edsonarios <edsonrios9@gmail.com>
2024-04-19 19:33:35 -0400

    Added basic translations

---
+++ src/components/i18n/ui.ts
@@ -0,0 +1,39 @@
+// Add missing imports
+export const LANGUAGES: Record<
+	string,
+	{ code: string; name: string; }
+> = {
+	en: {
+		code: 'en',
+		name: 'English',
+	},
+	es: {
+		code: 'es',
+		name: 'Español',
+	},
+};
+
+export const defaultLang = 'es';
+export const showDefaultLang = false;
+
+export const ui = {
+	es: {
+		'nav.inicio': 'Inicio',
+		'nav.docs': 'Vota',
+	},
+	en: {
+		'nav.inicio': 'Home',
+		'nav.docs': 'Vote',
+	},
+} as const;
+
+export const routes = {
+	es: {
+		docs: 'docs',
+		info: 'info',
+	},
+	en: {
+		docs: 'docs',
+		info: 'information',
+	},
+};
