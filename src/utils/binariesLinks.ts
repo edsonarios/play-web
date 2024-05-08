9bc26007db103898091f85641f99d763c27759b9
edsonarios <edsonrios9@gmail.com>
2024-05-07 21:07:23 -0400

    App Launched, fix wave errors, update links

---
+++ src/utils/binariesLinks.ts
@@ -0,0 +1,4 @@
+const version = '1.0.0'
+export const windowsLink = `https://github.com/edsonarios/play/releases/download/${version}/Play-Windows-${version}.exe`
+export const macLink = `https://github.com/edsonarios/play/releases/download/${version}/Play-Mac-${version}-universal.dmg`
+export const linuxLink = `https://github.com/edsonarios/play/releases/download/${version}/Play-Linux-${version}.deb`
--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,1 +1,3 @@
 export const appName = 'Play'
+export const urlMainWeb = 'https://edevv.dev/'
+export const urlPlayWeb = 'https://play.edevv.dev/'
